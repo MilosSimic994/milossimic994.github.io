82d9f57fe2714f5bb1f796dca2fb1aa0ce37c7ad
MilosSimic994 <60389147+MilosSimic994@users.noreply.github.com>
2021-04-06 20:38:25 +0200

    add shadow

--- src/app.js
+++ src/app.js
@@ -1,36 +1,25 @@
-const navLinks = document.querySelectorAll('.nav__links');
-const navList = document.querySelector('.list')
-const toggleMenu = document.querySelector('.nav__hamb');
-const navHolder = document.querySelector('.nav')
-
-
-
-
-
-
-
+const navLinks = document.querySelectorAll(".nav__links");
+const navList = document.querySelector(".list");
+const toggleMenu = document.querySelector(".nav__hamb");
+const navHolder = document.querySelector(".nav");
 
 navLinks.forEach((link) => {
-    link.addEventListener('mouseover', (e) => {
-        e.currentTarget.innerHTML = link.dataset.txt;
-    })
+  link.addEventListener("mouseover", (e) => {
+    e.currentTarget.innerHTML = link.dataset.txt;
+  });
 
+  link.addEventListener("mouseout", (e) => {
+    e.currentTarget.innerHTML = "";
+    let img = document.createElement("img");
+    img.src = `/images/${link.dataset.txt}.png`;
 
-    link.addEventListener('mouseout', (e) => {
-        e.currentTarget.innerHTML = ''
-        let img = document.createElement('img')
-        img.src = `/images/${link.dataset.txt}.png`
+    link.appendChild(img);
+  });
+});
 
-        link.appendChild(img)
-
-    })
-})
-
-
-toggleMenu.addEventListener('click', () => {
-    const height = navHolder.getBoundingClientRect().height;
-    console.log(height);
-    navList.style.top = `${height}px`
-    navList.classList.toggle('toggle-hamb')
-})
-
+toggleMenu.addEventListener("click", () => {
+  const height = navHolder.getBoundingClientRect().height;
+  console.log(height);
+  navList.style.top = `${height}px`;
+  navList.classList.toggle("toggle-hamb");
+});
