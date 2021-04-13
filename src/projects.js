c572765a4f07b938e1ac093f7a8ff590070ba727
MilosSimic994 <60389147+MilosSimic994@users.noreply.github.com>
2021-04-13 11:01:53 +0200

    add project desc

--- src/projects.js
+++ src/projects.js
@@ -4,7 +4,7 @@ const projects = [
   {
     title: "Todo App",
     desc:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur perspiciatis, est corrupti mollitia asperiores, neque debitis saepe alias rerum corporis sit officiis deserunt blanditiis explicabo, provident eius libero. ",
+      "Application for creating a todo list in light and dark versions, also allows you to view completed tasks, those that are waiting, also allows you to delete and create a new list.",
     tool: ["JavaScript", "HTML", "SCSS"],
     img: "/images/todo.jpg",
     site: "https://serene-kare-2096e9.netlify.app/",
@@ -13,16 +13,16 @@ const projects = [
   {
     title: "frigo",
     desc:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur perspiciatis, est corrupti mollitia asperiores, neque debitis saepe alias rerum corporis sit officiis deserunt blanditiis explicabo, provident eius libero. ",
+      "The client had a Joomla website, which needed a complete “makeover”. My job was to redesign it, add new content, and migrate it to WordPress CMS. The new website was built with Themify. ",
     tool: ["WordPress", "Themify", "CMS"],
     img: "/images/frigo1.jpg",
     site: "https://frigoservis.rs/",
     code: "newWork.html",
   },
   {
-    title: "Bambo",
+    title: "Bamboo",
     desc:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur perspiciatis, est corrupti mollitia asperiores, neque debitis saepe alias rerum corporis sit officiis deserunt blanditiis explicabo, provident eius libero. ",
+      "Mastercraft bamboo monitor riser is a landing page for a fictitious client that offers a choice of multiple products, shows the number of available products and the ability to order. ",
     tool: ["JavaScript", "HTML", "CSS"],
     img: "/images/bambo.jpg",
     site: "https://upbeat-shirley-7aed7d.netlify.app/",
@@ -31,7 +31,7 @@ const projects = [
   {
     title: "Hemera Veleprodaja",
     desc:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur perspiciatis, est corrupti mollitia asperiores, neque debitis saepe alias rerum corporis sit officiis deserunt blanditiis explicabo, provident eius libero. ",
+      "Hemera is a wholesale of cosmetics, the purpose of the website is to show part of the offer as well as the entire price list for existing customers with secured access. ",
     tool: ["JavaScript", "HTML", "CSS"],
     img: "/images/hemera.png",
     site: "http://vpkucnahemijahemera.rs/",
@@ -40,7 +40,7 @@ const projects = [
   {
     title: "Manage Agency",
     desc:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur perspiciatis, est corrupti mollitia asperiores, neque debitis saepe alias rerum corporis sit officiis deserunt blanditiis explicabo, provident eius libero. ",
+      "Manage Agency is a landing page for a fictitious client with a nice slider to display testimonials. ",
     tool: ["SCSS", "HTML", "CSS"],
     img: "/images/manage.png",
     site: "https://goofy-minsky-7233db.netlify.app/",
@@ -49,7 +49,7 @@ const projects = [
   {
     title: "Pizza Bar",
     desc:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur perspiciatis, est corrupti mollitia asperiores, neque debitis saepe alias rerum corporis sit officiis deserunt blanditiis explicabo, provident eius libero. ",
+      "Pizza bar is a landing page with an interesting look, with a lot of overlapping elements, this project was made for a fictitious client  ",
     tool: ["SCSS", "HTML", "CSS"],
     img: "/images/whats-new-pizza.jpg",
     site: "https://zealous-visvesvaraya-beea78.netlify.app/",
@@ -93,11 +93,9 @@ console.log(imgs);
 function resizeImg() {
   imgs.forEach((img) => {
     img.addEventListener("mouseover", (e) => {
-      console.log("ana");
       e.target.classList.add("resize");
     });
     img.addEventListener("mouseout", (e) => {
-      console.log("ana");
       e.target.classList.remove("resize");
     });
   });
