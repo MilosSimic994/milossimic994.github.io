1a4d5d3f96c4d222b5d5723880ce4326a2a1e315
MilosSimic994 <60389147+MilosSimic994@users.noreply.github.com>
2021-06-15 11:09:27 +0200

    new project

--- src/projects.js
+++ src/projects.js
@@ -3,17 +3,23 @@ const projectSection = document.querySelector(".project-wrapper");
 const projects = [
   {
     title: "paper, rock, scissors",
-    desc:
-      "Try to beat the computer in this interesting game, if you forgot the rules you can remember in the lower right corner. if you want to take a break and come back with better tactics that's fine because the result will remain the same.",
+    desc: "Try to beat the computer in this interesting game, if you forgot the rules you can remember in the lower right corner. if you want to take a break and come back with better tactics that's fine because the result will remain the same.",
     tool: ["JavaScript", "localStorage", "SCSS"],
     img: "/images/paperRockSciss.jpg",
     site: "https://rock-paper-sciss.netlify.app/",
     code: "https://github.com/MilosSimic994/to-do-app",
   },
   {
+    title: "Exchange Rate Calculator",
+    desc: "Try to beat the computer in this interesting game, if you forgot the rules you can remember in the lower right corner. if you want to take a break and come back with better tactics that's fine because the result will remain the same.",
+    tool: ["JavaScript", "localStorage", "SCSS"],
+    img: "/images/exchange.png",
+    site: "https://exchange-rate-m.netlify.app/",
+    code: "https://github.com/MilosSimic994/Exchange-Rate",
+  },
+  {
     title: "Todo App",
-    desc:
-      "Application for creating a todo list in light and dark versions, also allows you to view completed tasks, those that are waiting, also allows you to delete and create a new list.",
+    desc: "Application for creating a todo list in light and dark versions, also allows you to view completed tasks, those that are waiting, also allows you to delete and create a new list.",
     tool: ["JavaScript", "HTML", "SCSS"],
     img: "/images/todo.jpg",
     site: "https://serene-kare-2096e9.netlify.app/",
@@ -21,8 +27,7 @@ const projects = [
   },
   {
     title: "frigo",
-    desc:
-      "The client had a Joomla website, which needed a complete “makeover”. My job was to redesign it, add new content, and migrate it to WordPress CMS. The new website was built with Themify. ",
+    desc: "The client had a Joomla website, which needed a complete “makeover”. My job was to redesign it, add new content, and migrate it to WordPress CMS. The new website was built with Themify. ",
     tool: ["WordPress", "Themify", "CMS"],
     img: "/images/frigo1.jpg",
     site: "https://frigoservis.rs/",
@@ -30,8 +35,7 @@ const projects = [
   },
   {
     title: "Bamboo",
-    desc:
-      "Mastercraft bamboo monitor riser is a landing page for a fictitious client that offers a choice of multiple products, shows the number of available products and the ability to order. ",
+    desc: "Mastercraft bamboo monitor riser is a landing page for a fictitious client that offers a choice of multiple products, shows the number of available products and the ability to order. ",
     tool: ["JavaScript", "HTML", "CSS"],
     img: "/images/bambo.jpg",
     site: "https://upbeat-shirley-7aed7d.netlify.app/",
@@ -39,8 +43,7 @@ const projects = [
   },
   {
     title: "Hemera Veleprodaja",
-    desc:
-      "Hemera is a wholesale of cosmetics, the purpose of the website is to show part of the offer as well as the entire price list for existing customers with secured access. ",
+    desc: "Hemera is a wholesale of cosmetics, the purpose of the website is to show part of the offer as well as the entire price list for existing customers with secured access. ",
     tool: ["JavaScript", "HTML", "CSS"],
     img: "/images/hemera.png",
     site: "http://vpkucnahemijahemera.rs/",
@@ -48,8 +51,7 @@ const projects = [
   },
   {
     title: "Manage Agency",
-    desc:
-      "Manage Agency is a landing page for a fictitious client with a nice slider to display testimonials. ",
+    desc: "Manage Agency is a landing page for a fictitious client with a nice slider to display testimonials. ",
     tool: ["SCSS", "HTML", "CSS"],
     img: "/images/manage.png",
     site: "https://goofy-minsky-7233db.netlify.app/",
@@ -57,8 +59,7 @@ const projects = [
   },
   {
     title: "Pizza Bar",
-    desc:
-      "Pizza bar is a landing page with an interesting look, with a lot of overlapping elements, this project was made for a fictitious client  ",
+    desc: "Pizza bar is a landing page with an interesting look, with a lot of overlapping elements, this project was made for a fictitious client  ",
     tool: ["SCSS", "HTML", "CSS"],
     img: "/images/whats-new-pizza.jpg",
     site: "https://zealous-visvesvaraya-beea78.netlify.app/",
